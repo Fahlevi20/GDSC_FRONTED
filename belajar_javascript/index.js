42c91928a9de33ea3724c616cfc18bb75cf9aec2
Fahlevi20 <rozakalfi@gmail.com>
2024-05-17 20:11:40 +0700

    if else and loop for

--- belajar_javascript/index.js
+++ belajar_javascript/index.js
@@ -1,18 +1,33 @@
-let x = 10;
-let y = 5;
+// let x = 10;
+// let y = 5;
 
-console.log(x+y);
-console.log(x-y);
-console.log(x*y);
-console.log(x/y);
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
 
-console.log(x>y);
-console.log(x==y);
-console.log(x<y);
+// console.log(x>y);
+// console.log(x==y);
+// console.log(x<y);
 
-let isAult = true;
-let hasLicense = false;
+// let isAult = true;
+// let hasLicense = false;
 
-console.log(isAult && hasLicense);
-console.log(isAult || hasLicense);
-console.log(!isAult);
+// console.log(isAult && hasLicense);
+// console.log(isAult || hasLicense);
+// console.log(!isAult);
+
+// condition
+
+let age = 18;
+if(age >= 18){
+    console.log("is ault");
+}else{
+    console.log('under age');
+}
+
+// looping for
+
+for (let i=1; i<5; i++){
+    console.log(i)
+}
